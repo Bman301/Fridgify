a0dab818df94e0b63140a9a966d2e75f67dab8ab
ramirez <prlara2000@gmail.com>
2021-06-01 15:57:36 -0500

    add the controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :recipes, only: [ :index, :show, :new, :create ]
-  resources :cookbooks, only: [ :index, :new, :create ]
-  resources :ratings, only: [ :new, :create ]
+  resources :recipes, only: [ :index, :show, :new, :create ] do
+    resources :ratings, only: [:create ]
+    resources :cookbooks, only: [ :create ]
+  end
+
+  resources :cookbooks, only: [:index, :destroy]
+
+
 
 
 end
