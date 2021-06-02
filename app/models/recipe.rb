b53cbae5033aaed24a8c5c66816b141b256fbdc2
ramirez <prlara2000@gmail.com>
2021-06-02 12:30:44 -0500

    adding the search

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,16 @@
 class Recipe < ApplicationRecord
+
+  include PgSearch::Model
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
+  pg_search_scope :global_search,
+    associated_against: {
+      ingredient: :name
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
+
 end
