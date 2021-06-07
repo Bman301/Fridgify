e94c7c9a295e8efe1333e643a23f3e414b6e4603
Brayan Rodriguez <panda99161@gmail.com>
2021-06-07 10:24:36 -0500

    Add steps to recipe table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_173619) do
+ActiveRecord::Schema.define(version: 2021_06_07_151231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_173619) do
     t.string "difficulty_level"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "steps"
   end
 
   create_table "users", force: :cascade do |t|
