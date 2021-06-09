0c43d793f14f42101e744e89d41153841c98fe8a
Titch F <github@fauconnier.eu>
2021-06-09 19:06:23 +0200

    ddd

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -10,26 +10,19 @@ class CookbooksController < ApplicationController
 
 
   def create
-
     @cookbook = Cookbook.find_by(recipe_id: params[:recipe_id], user_id: current_user.id)
-    if @cookbook 
-    
+    if @cookbook
       @cookbook.destroy
       render json: {}
-    
     else
-      
         @cookbook = Cookbook.new
         @user = current_user
         @recipe = Recipe.find(params[:recipe_id])
-
         @cookbook.recipe = @recipe
         @cookbook.user = @user
-
         @cookbook.save
           render json: @cookbook
     end
   end
-
 
 end
