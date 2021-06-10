359306a58965726f1f0d3cc4d18f9e79689bcff2
Titch F <github@fauconnier.eu>
2021-06-10 18:09:14 +0200

    ddd

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,7 @@ class RecipesController < ApplicationController
   def index
 
     user_ingredients = params[:query].split(" ")
+    @user_ingredients = user_ingredients
     @recipes = []
     user_ingredients.each do |ingredient|
       @recipes << Recipe.by_ingredient(ingredient)
